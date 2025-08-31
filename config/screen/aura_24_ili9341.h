d5b072e9642e2ec408faf503dda4c2974484c873
Andrew Oakes <112913282+OakesekAo@users.noreply.github.com>
2025-08-31 21:51:58 +0000

    config: define AURA_TFT_{WIDTH,HEIGHT,BL} in screen headers; relative includes

--- config/screen/aura_24_ili9341.h
+++ config/screen/aura_24_ili9341.h
@@ -1,4 +1,8 @@
 #pragma once
-#define SCREEN_WIDTH  320
-#define SCREEN_HEIGHT 240
+#define AURA_TFT_WIDTH   320
+#define AURA_TFT_HEIGHT  240
+#ifndef TFT_BL
+	#define TFT_BL 21
+#endif
+#define AURA_TFT_BL      TFT_BL
 #define AURA_TFT_HEIGHT 320
